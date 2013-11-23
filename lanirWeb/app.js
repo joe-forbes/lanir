307cfbbd614cdadcce7445a03c6c92d068cb8a08
Joe Forbes <joe.forbes@utexas.edu>
2013-11-22 23:33:37 -0600

    Unbelievably, may have the api finished already.

--- lanirWeb/app.js
+++ lanirWeb/app.js
@@ -10,8 +10,16 @@ console.log("lirc_node initialized.");
 app.listen(port);
 console.log("lanirWeb listening on port " + port + ".");
 
-
 app.get('/', function(req, res) {
 	console.log("returning list of remotes");
 	res.send(lirc_node.remotes);
+});
+
+app.get('/:remote/:command', function(req, res) {
+	console.log("in send function");
+	console.log("remote = " + req.params.remote);
+	console.log("command = " + req.params.command);
+	lirc_node.irsend.send_once(req.params.remote, req.params.command, function() {});
+	res.setHeader('Cache-Control', 'no-cache');
+	res.send(200);
 });
