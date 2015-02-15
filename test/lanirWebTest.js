9ace8bcafc061a9bc2b83502d050385daec99112
Joe Forbes <joe.forbes@utexas.edu>
2015-02-14 21:27:16 -0600

    Starting 0.1.0. Goals for that are to walk over all the stuff from "node" scratch project.
    So far, switched to supertest and should, dropped grunt and other unused
    modules. Added blanket.

--- test/lanirWebTest.js
+++ test/lanirWebTest.js
@@ -1,60 +1,48 @@
-var app = require('../app');
-var assert = require('assert');
+var server = require('../server');
 var request = require('supertest');
-var sinon = require('sinon');
 
+require("should");
+require("blanket");
 
 describe('lanirWeb', function() {
 
-    describe('routes', function() {
+	describe('routes', function() {
 
-        it('should have an index route accessible via GET', function(done) {
-            assert(request(app).get('/').expect(200, done));
-        });
+		it('should have an index route accessible via GET', function(done) {
+			request(server).get('/').expect(200, done);
+		});
 
-        it('should have an API route accessible via POST', function(done) {
-            assert(request(app).post('/remotes/tv/power').expect(200, done));
-        });
+		it('should have an API route accessible via POST', function(done) {
+			request(server).post('/remotes/tv/power').expect(200, done);
+		});
 
-    });
+	});
 
-    describe('index action', function() {
+	describe('index action', function() {
 
-        it('should return JSON', function(done) {
-            assert(request(app).get('/').expect('Content-Type', /application\/json/, done));
-        });
+		it('should return JSON', function(done) {
+			request(server).get('/').expect('Content-Type',  /application\/json/, done);
+		});
 
-        it('should return JSON in the format { remotes: [{name: ..., commands: [...]}]}', function(done) {
-			request(app)
-			.get('/')
-			.end(function(err, res) {
-				assert.equal(err, null);
-				json = JSON.parse(res.text);
-				var keys = Object.keys(json);
-				assert(keys.indexOf('remotes') != -1);
-				assert(isArray(json.remotes));
-				for (var i in json.remotes) {
-					var remote = json.remotes[i];
-					keys = Object.keys(remote);
-					assert(keys.indexOf('name') != -1);
-					assert(!isArray(remote.name));
-					assert(typeof remote.name != 'object');
-					assert(keys.indexOf('commands') != -1);
-					assert(isArray(remote.commands));
-					for (var s in remote.commands) {
-						var string = remote.commands[s];
-						assert(!isArray(string));
-						assert(typeof string != 'object');
+		it('should return JSON in the format { remotes: [{name: ..., commands: [...]}]}', function(done) {
+			request(server).get('/').end(function(err, res) {
+				if (err) {
+					throw err;
+				}
+				response = JSON.parse(res.text);
+				response.should.have.property("remotes").which.is.instanceOf(Array);
+				for ( var i in response.remotes) {
+					var remote = response.remotes[i];
+					remote.should.have.property("name").which.is.of.type("string");
+					remote.should.have.property("commands").which.is.instanceOf(Array);
+					for ( var s in remote.commands) {
+						remote.commands[s].should.be.of.type("string");
 					}
 				}
-			    done();
+				done();
 			});
-        });
+		});
 
-    });
+	});
 
 });
-
-function isArray(what) {
-    return Object.prototype.toString.call(what) === '[object Array]';
-}
